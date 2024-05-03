99f4f43ae7ed0138b4fe707f2140216d8ac8136e
yhoogstrate <y.hoogstrate@gmail.com>
2024-05-03 11:54:45 +0200

    updates

--- parse_idat.py
+++ parse_idat.py
@@ -11,6 +11,7 @@ import re
 
 from beartype import beartype
 from _io import BufferedReader
+from _io import BufferedWriter
 
 import numpy as np
 from numpy import ndarray
@@ -42,32 +43,10 @@ section_names = {
     1000: 'ARRAY_N_PROBES'
 }
 
-section_locations = {
-    'PROBE_IDS': 102,
-    'PROBE_STD_DEVS': 103,
-    'PROBE_MEAN_INTENSITIES': 104,
-    'PROBE_N_BEADS': 107,
-    'PROBE_MID_BLOCK': 200,
-    'ARRAY_RUN_INFO': 300,
-    'ARRAY_RED_GREEN': 400,
-    'ARRAY_MANIFEST': 401,
-    'ARRAY_BARCODE': 402,
-    'ARRAY_CHIP_TYPE': 403,
-    'ARRAY_CHIP_LABEL': 404,
-    'ARRAY_OLD_STYLE_MANIFEST': 405,
-    'ARRAY_SAMPLE_ID': 406,
-    'ARRAY_DESCRIPTION': 407,
-    'ARRAY_PLATE': 408,
-    'ARRAY_WELL': 409,
-    'ARRAY_UNKNOWN_1': 410,
-    'ARRAY_UNKNOWN_2': 510,
-    'ARRAY_N_PROBES': 1000
-}
 
 
+class IDATdata(object):
 
-
-class IDATdata:
     def __init__(self):
         self.data_file_magic = None
         self.data_idat_version = None
@@ -266,11 +245,11 @@ class IDATdata:
 
 
 
-class IDATfile(IDATdata):
+class IDATreader:
 
     @beartype
     def __init__(self, idat_filename: Path):
-        IDATdata.__init__(self)
+        self.data = IDATdata()
         
         self.idat_filename = idat_filename
         self.parse()
@@ -279,13 +258,13 @@ class IDATfile(IDATdata):
     def parse_file_magic(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['FILE_MAGIC'])
         
-        return self.set_file_magic(read_char(fh_in, 4))
+        return self.data.set_file_magic(read_char(fh_in, 4))
     
     @beartype
     def parse_idat_version(self, fh_in: BufferedReader, section_seek_index: dict) -> int:
         fh_in.seek(section_seek_index['IDAT_VERSION'])
         
-        return self.set_idat_version(read_long(fh_in))
+        return self.data.set_idat_version(read_long(fh_in))
         
     @beartype
     def parse_section_index(self, fh_in: BufferedReader, section_seek_index: dict) -> dict:
@@ -305,24 +284,24 @@ class IDATfile(IDATdata):
             section_order.append(section_type)
             section_seek_index[section_type] = read_long(fh_in)
 
-        self.set_section_order(section_order)
+        self.data.set_section_order(section_order)
         return section_seek_index
     
     @beartype
     def parse_array_n_probes(self, fh_in: BufferedReader, section_seek_index: dict) -> int:
         fh_in.seek(section_seek_index['ARRAY_N_PROBES'])
         
-        return self.set_array_n_probes(read_int(fh_in))
+        return self.data.set_array_n_probes(read_int(fh_in))
 
 
     @beartype
     def parse_probe_ids(self, fh_in: BufferedReader, section_seek_index: dict) -> ndarray:
         fh_in.seek(section_seek_index['PROBE_IDS'])
         
-        if self.data_array_n_probes is None:
+        if self.data.data_array_n_probes is None:
             self.parse_array_n_probes(fh_in, section_seek_index)
         
-        probe_ids = npread(fh_in, '<u4', self.data_array_n_probes) # layout-check: (4207470- 210) / 1051815 = 4
+        probe_ids = npread(fh_in, '<u4', self.data.data_array_n_probes) # layout-check: (4207470- 210) / 1051815 = 4
         
         if np.any(probe_ids <= 0):
             raise Exception("Wrong probe id's found")
@@ -336,10 +315,10 @@ class IDATfile(IDATdata):
     def parse_probe_std_devs(self, fh_in: BufferedReader, section_seek_index: dict) -> ndarray:
         fh_in.seek(section_seek_index['PROBE_STD_DEVS'])
         
-        if self.data_array_n_probes is None:
+        if self.data.data_array_n_probes is None:
             self.parse_array_n_probes(fh_in, section_seek_index)
         
-        probe_std_devs = npread(fh_in, '<u2', self.data_array_n_probes) # layout-check: (6311100 - 4207470) / 1051815 = 2
+        probe_std_devs = npread(fh_in, '<u2', self.data.data_array_n_probes) # layout-check: (6311100 - 4207470) / 1051815 = 2
         
         if np.any(probe_std_devs < 0):
             raise Exception("Wrong std dev found (0 or negative)")
@@ -350,10 +329,10 @@ class IDATfile(IDATdata):
     def parse_probe_mean_intensities(self, fh_in: BufferedReader, section_seek_index: dict) -> ndarray:
         fh_in.seek(section_seek_index['PROBE_MEAN_INTENSITIES'])
         
-        if self.data_array_n_probes is None:
+        if self.data.data_array_n_probes is None:
             self.parse_array_n_probes(fh_in, section_seek_index)
         
-        probe_mean_intensities = npread(fh_in, '<u2', self.data_array_n_probes) # layout-check: (8414730 - 6311100) / 1051815 = 2
+        probe_mean_intensities = npread(fh_in, '<u2', self.data.data_array_n_probes) # layout-check: (8414730 - 6311100) / 1051815 = 2
         
         if np.any(probe_mean_intensities < 0):
             raise Exception("Wrong median probe intensity found (negative)")
@@ -364,10 +343,10 @@ class IDATfile(IDATdata):
     def parse_probe_n_beads(self, fh_in: BufferedReader, section_seek_index: dict) -> ndarray:
         fh_in.seek(section_seek_index['PROBE_N_BEADS'])
         
-        if self.data_array_n_probes is None:
+        if self.data.data_array_n_probes is None:
             self.parse_array_n_probes(fh_in, section_seek_index)
         
-        probe_n_beads = npread(fh_in, '<u1', self.data_array_n_probes) # layout-check: (9466545 - 8414730) / 1051815 = 1
+        probe_n_beads = npread(fh_in, '<u1', self.data.data_array_n_probes) # layout-check: (9466545 - 8414730) / 1051815 = 1
         
         if np.any(probe_n_beads < 0):
             raise Exception("Wrong number of beads per probe found (0 or negative)")
@@ -378,13 +357,13 @@ class IDATfile(IDATdata):
     def parse_probe_mid_block(self, fh_in: BufferedReader, section_seek_index: dict) -> ndarray:
         fh_in.seek(section_seek_index['PROBE_MID_BLOCK'])
         
-        if self.data_array_n_probes is None:
+        if self.data.data_array_n_probes is None:
             self.parse_array_n_probes(fh_in, section_seek_index)
         
-        if self.data_array_n_probes != read_int(fh_in):
+        if self.data.data_array_n_probes != read_int(fh_in):
             raise Exception("Weird discrepancy between number of probes and size of mid block")
         
-        probe_mid_block = npread(fh_in, '<u4', self.data_array_n_probes) # layout-check: (13673809 - (9466545 + 4)) / 1051815 = 4
+        probe_mid_block = npread(fh_in, '<u4', self.data.data_array_n_probes) # layout-check: (13673809 - (9466545 + 4)) / 1051815 = 4
         
         if np.any(probe_mid_block <= 0):
             raise Exception("Wrong probe id's found")
@@ -409,7 +388,7 @@ class IDATfile(IDATdata):
 
         self.data_per_probe_matrix = data_per_probe_matrix
 
-        return self.set_data_per_probe_matrix(data_per_probe_matrix)
+        return self.data.set_data_per_probe_matrix(data_per_probe_matrix)
 
     @beartype
     def parse_array_red_green(self, fh_in: BufferedReader, section_seek_index: dict) -> int:
@@ -420,73 +399,73 @@ class IDATfile(IDATdata):
         #if red_green != 0:
         #    raise Exception("Only seen 0 so far, but probably good...")
         
-        return self.set_array_red_green(red_green)
+        return self.data.set_array_red_green(red_green)
 
     @beartype
     def parse_array_manifest(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_MANIFEST'])
         
-        return self.set_array_manifest(read_string(fh_in))
+        return self.data.set_array_manifest(read_string(fh_in))
     
     @beartype
     def parse_array_barcode(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_BARCODE'])
         
-        return self.set_array_barcode(read_string(fh_in))
+        return self.data.set_array_barcode(read_string(fh_in))
 
     @beartype
     def parse_array_chip_type(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_CHIP_TYPE'])
         
-        return self.set_array_chip_type(read_string(fh_in))
+        return self.data.set_array_chip_type(read_string(fh_in))
 
     @beartype
     def parse_array_chip_label(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_CHIP_LABEL'])
         
-        return self.set_array_chip_label(read_string(fh_in))
+        return self.data.set_array_chip_label(read_string(fh_in))
 
     @beartype
     def parse_array_old_style_manifest(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_OLD_STYLE_MANIFEST'])
         
-        return self.set_array_old_style_manifest(read_string(fh_in))
+        return self.data.set_array_old_style_manifest(read_string(fh_in))
 
     @beartype
     def parse_array_unknown_1(self, fh_in: BufferedReader, section_seek_index: dict) -> tuple[int, int, int, int]:
         fh_in.seek(section_seek_index['ARRAY_UNKNOWN_1'])
         
-        return self.set_array_unknown_1((read_byte(fh_in), read_byte(fh_in), read_byte(fh_in), read_byte(fh_in)))
+        return self.data.set_array_unknown_1((read_byte(fh_in), read_byte(fh_in), read_byte(fh_in), read_byte(fh_in)))
 
     @beartype
     def parse_array_sample_id(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_SAMPLE_ID'])
         
-        return self.set_array_sample_id(read_string(fh_in))
+        return self.data.set_array_sample_id(read_string(fh_in))
 
     @beartype
     def parse_array_description(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_DESCRIPTION'])
         
-        return self.set_array_description(read_string(fh_in))
+        return self.data.set_array_description(read_string(fh_in))
 
     @beartype
     def parse_array_plate(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_PLATE'])
         
-        return self.set_array_plate(read_string(fh_in))
+        return self.data.set_array_plate(read_string(fh_in))
 
     @beartype
     def parse_array_well(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_WELL'])
         
-        return self.set_array_well(read_string(fh_in))
+        return self.data.set_array_well(read_string(fh_in))
 
     @beartype
     def parse_array_unknown_2(self, fh_in: BufferedReader, section_seek_index: dict) -> str:
         fh_in.seek(section_seek_index['ARRAY_UNKNOWN_2'])
         
-        return self.set_array_unknown_2(read_string(fh_in))
+        return self.data.set_array_unknown_2(read_string(fh_in))
     
     @beartype
     def parse_array_run_info(self, fh_in: BufferedReader, section_seek_index: dict) -> list[tuple[str, str, str, str, str]]:
@@ -497,7 +476,7 @@ class IDATfile(IDATdata):
         for i in range(read_int(fh_in)): # blocks containing 5 consequtive strings
             run_info.append((read_string(fh_in), read_string(fh_in), read_string(fh_in), read_string(fh_in), read_string(fh_in)))
         
-        return self.set_array_run_info(run_info)
+        return self.data.set_array_run_info(run_info)
 
     @beartype
     def parse(self) -> int:
@@ -526,197 +505,38 @@ class IDATfile(IDATdata):
             self.parse_array_well(fh_in, section_seek_index)
             self.parse_array_unknown_2(fh_in, section_seek_index)
             self.parse_array_run_info(fh_in, section_seek_index)
-        
+
         return 0
 
 
-d_red = IDATfile(Path("GSM6379997_203927450093_R01C01_Grn.idat"))
-d_grn = IDATfile(Path("GSM6379997_203927450093_R01C01_Red.idat"))
 
+class IDATwriter(IDATdata):
 
-print(d_red)
-
-import sys
-sys.exit()
-
-
-def get_magic(fh_in):
-    fh_in.seek(0) # IdatHeaderLocation.FILE_TYPE.value
-    file_type = read_char(fh_in, 4)
-    
-    return file_type
-
-def get_idat_version(fh_in):
-    fh_in.seek(4) # IdatHeaderLocation.VERSION.value
-    print("    => [" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"]")
-
-    fh_in.seek(4) # IdatHeaderLocation.VERSION.value
-    idat_version = read_long(fh_in)
-    
-    return idat_version
-
-
-def get_section_offsets(fh_in):
-    """Parses the IDAT file header to get the byte position
-    for the start of each section.
-
-    Arguments:
-        fh_in {file-like} -- the IDAT file to process.
-
-    Returns:
-        [dict] -- The byte offset for each file section.
-    """
-    fh_in.seek(12) # IdatHeaderLocation.FIELD_COUNT.value
-    num_fields = read_int(fh_in)
-
-    fh_in.seek(16) # IdatHeaderLocation.SECTION_OFFSETS.value
-    pos = 16
-    poss = []
-
-    offsets = {}
-    for _idx in range(num_fields):
-        poss.append(str(pos) + "-" + str(pos+9))
-        
-        key = read_short(fh_in)
-        offsets[read_long(fh_in)] = key
-        
-        pos += 2 + 8
-
-    print("    => "+str(poss))
-
-    return offsets
-
-
-with open(Path("GSM6379997_203927450093_R01C01_Grn.idat"), "rb") as fh_in:
-    print("1.  Magic:        ["+get_magic(fh_in)+"]")
-    print("2.  IDAT version: ["+str(get_idat_version(fh_in))+"]")
-    print("3.  section offsets:")
-    
-    offsets = get_section_offsets(fh_in)
-    
-    print("4.  details")
-    for offset in sorted(offsets):
-        key = offsets[offset]
-        
-        if key in section_names:
-            print("    " + str(key) + ": " + str(offset) + "    ("+section_names[key]+")")
+    @beartype
+    def __init__(self, idat_data: IDATdata):
+        if isinstance(idat_data, IDATdata):
+            self.data = idat_data
+        elif isinstance(idat_data, IDATreader):
+            self.data = idat_data.data
+        elif isinstance(idat_data, IDATwriter):
+            self.data = idat_data.data
         else:
-            print("    " + str(key) + ": " + str(offset))
-        
-        if key == 102:
-            pass # just value parsing
-        elif key == 103:
-            pass # just value parsing
-        elif key == 104:
-            pass # just value parsing
-        elif key == 107:
-            pass # just value parsing
-            #seek_to_section(IdatSectionCode.PROBE_N_BEADS)
-            #self.n_beads = npread(idat_file, '<u1', self.n_snps_read) # was <u1
+            raise Exception("Unclear input type")
 
-        elif key == 200:
-            fh_in.seek(offset)
-            midblock = []
-            for i in range(read_int(fh_in)):
-                midblock.append(read_int(fh_in))
-            
-            midblock = np.array(midblock)
-            print("    => len: " + str(len(midblock)) + ",  [" + str(midblock[0]) + ", "+str(midblock[1]) + ", ..., "  + str(midblock[-2]) + ", "+str(midblock[-1]) + "]")
+    @beartype
+    def write(self, idat_filename: Path):
+        with open(idat_filename, 'wb') as fh_out:
+            write_char(fh_out, self.data.data_file_magic)
 
 
-        elif key == 300:
-            fh_in.seek(offset)
-            for i in range(read_int(fh_in)):
-                print("    => "+str(i+1)+". [" + str(read_string(fh_in))+"][" + str(read_string(fh_in))+"][" + str(read_string(fh_in))+"][" + str(read_string(fh_in))+"][" + str(read_string(fh_in))+"]")
-            
-        elif key == 400:
-            fh_in.seek(offset)
-            print("    => [" + str(read_int(fh_in))+"]")
-        elif key == 401:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 402:
-            fh_in.seek(offset)
-            print("    => [" + str(read_string(fh_in))+"]")
-        elif key == 403:
-            fh_in.seek(offset)
-            print("    => [" + str(read_string(fh_in))+"]")
-        elif key == 404:
-            fh_in.seek(offset)
-            print("    => [" + str(read_string(fh_in))+"]")
-        elif key == 405:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 406:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 407:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 408:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 409:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 410:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"][" + str(read_byte(fh_in))+"]")
-        elif key == 510:
-            fh_in.seek(offset)
-            print("    => [" + str(read_byte(fh_in))+"]")
-        elif key == 1000:
-            fh_in.seek(offset)
-            print("    => [" + str(read_int(fh_in))+"]")
-        else:
-            raise Exception("Key: " + str(key) + " not yet implemented")
 
+d_red = IDATreader(Path("GSM6379997_203927450093_R01C01_Grn.idat"))
 
-    offset = [_ for _ in offsets if offsets[_] == section_locations['ARRAY_N_PROBES']][0]
-    fh_in.seek(offset)
-    n_snps_read = read_int(fh_in)
+w = IDATwriter(d_red.data)
+w.write(Path("test.idat"))
 
-    offset = [_ for _ in offsets if offsets[_] == section_locations['PROBE_N_BEADS']][0]
-    fh_in.seek(offset)
-    n_beads = npread(fh_in, '<u1', n_snps_read) # was <u1
-    
-    print("5. beads:  " + str(n_beads[0:24]) + "    (n="+str(len(n_beads))+")")
-    
-    
-    offset = [_ for _ in offsets if offsets[_] == section_locations['PROBE_IDS']][0]
-    fh_in.seek(offset)
-    illumn = npread(fh_in, '<u4', n_snps_read) # was <u1
-    
-    print("6. Illumina IDs:  " + str(illumn[0:8]) + "    (n="+str(len(illumn))+")")
-    print("   [midblock]  :  " + str(midblock[0:8]) + "    (n="+str(len(midblock))+")")
-    
-    if np.all(illumn == midblock):
-        print("   => idenictal")
-    else:
-        raise Exception("discrepancies between identifier blocks")
+#d_grn = IDATreader(Path("GSM6379997_203927450093_R01C01_Red.idat"))
 
-
-    offset = [_ for _ in offsets if offsets[_] == section_locations['PROBE_MEAN_INTENSITIES']][0]
-    fh_in.seek(offset)
-    PROBE_MEAN_INTENSITIES = npread(fh_in, '<u4', n_snps_read)
-    
-    print("7. PROBE_MEAN_INTENSITIESs:  " + str(PROBE_MEAN_INTENSITIES[0:7]) + "    (n="+str(len(PROBE_MEAN_INTENSITIES))+")")
-    
-    
-    offset = [_ for _ in offsets if offsets[_] == section_locations['PROBE_STD_DEVS']][0]
-    fh_in.seek(offset)
-    sd = npread(fh_in, '<u4', n_snps_read)
-    
-    print("8. Stddevs:  " + str(sd[0:7]) + "    (n="+str(len(sd))+")")
-    
-    
-    df = pd.DataFrame({
-                'probe_id': illumn,
-                'probe_n_beads': n_beads,
-                'probe_mean_intensities': PROBE_MEAN_INTENSITIES,
-                'probe_sd': sd })
-    
-    print(df)
 
 
 
--- parser.py
+++ parser.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+
 import numpy as np
 
-def bytes_to_int(input_bytes, signed=False):
+from beartype import beartype
+from _io import BufferedReader
+from _io import BufferedWriter
+
+
+@beartype
+def bytes_to_int(input_bytes, signed:bool=False) -> int:
     """Returns the integer represented by the given array of bytes.
     Pre-sets the byteorder to be little-endian.
 
@@ -21,97 +28,104 @@ def bytes_to_int(input_bytes, signed=False):
     return int.from_bytes(input_bytes, byteorder='little', signed=signed)
 
 
-
-def read_byte(infile):
+@beartype
+def read_byte(fh_in: BufferedReader) -> int:
     """Converts a single byte to an integer value.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
 
     Returns:
         [integer] -- Unsigned integer value converted from the supplied bytes.
     """
-    return bytes_to_int(infile.read(1), signed=False)
+    return bytes_to_int(fh_in.read(1), signed=False)
 
 
-def read_short(infile):
+@beartype
+def read_short(fh_in: BufferedReader) -> int:
     """Converts a two-byte element to an integer value.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
 
     Returns:
         [integer] -- Unsigned integer value converted from the supplied bytes.
     """
-    return bytes_to_int(infile.read(2), signed=False)
+    return bytes_to_int(fh_in.read(2), signed=False)
 
 
-def read_int(infile):
+@beartype
+def read_int(fh_in: BufferedReader) -> int:
     """Converts a four-byte element to an integer value.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
 
     Returns:
         [integer] -- Signed integer value converted from the supplied bytes.
     """
-    return bytes_to_int(infile.read(4), signed=True)
+    return bytes_to_int(fh_in.read(4), signed=True)
 
 
-def read_long(infile):
+@beartype
+def read_long(fh_in: BufferedReader) -> int:
     """Converts an eight-byte element to an integer value.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
 
     Returns:
         [integer] -- Signed integer value converted from the supplied bytes.
     """
-    return bytes_to_int(infile.read(8), signed=True)
+    return bytes_to_int(fh_in.read(8), signed=True)
 
 
-def read_char(infile, num_bytes):
+@beartype
+def read_char(fh_in: BufferedReader, num_bytes: int) -> str:
     """Converts an array of bytes to a string.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
         num_bytes {integer} -- The number of bytes to read and parse.
 
     Returns:
         [string] -- UTF-8 decoded string value.
     """
-    return infile.read(num_bytes).decode('utf-8')
+    return fh_in.read(num_bytes).decode('utf-8')
 
 
-def read_string(infile):
+@beartype
+def read_string(fh_in: BufferedReader) -> str:
     """Converts an array of bytes to a string.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
 
     Returns:
         [string] -- UTF-8 decoded string value.
     """
-    num_bytes = read_byte(infile)
+    num_bytes = read_byte(fh_in)
     num_chars = num_bytes % 128
     shift = 0
 
     while num_bytes // 128 == 1:
-        num_bytes = read_byte(infile)
+        num_bytes = read_byte(fh_in)
         shift += 7
         offset = (num_bytes % 128) * (2 ** shift)
         num_chars += offset
 
-    return read_char(infile, num_chars)
+    return read_char(fh_in, num_chars)
 
-def npread(file_like, dtype, n):
+
+@beartype
+def npread(fh_in: BufferedReader, dtype: str, n_elements: int):
     # https://stackoverflow.com/questions/72838939/how-to-convert-the-string-between-numpy-array-and-bytes
     """Parses a binary file multiple times, allowing for control if the
     file ends prematurely. This replaces read_results() and runs faster.
     And it provides support for reading gzipped idat files without decompressing.
 
     Arguments:
-        infile {file-like} -- The binary file to read the select number of bytes.
+        fh_in {file-like} -- The binary file to read the select number of bytes.
         dtype {data type} -- used within idat files, 2-bit, or 4-bit numbers stored in binary at specific addresses
         n {number of snps read} -- see files/idat.py for how this function is applied.
 
@@ -124,13 +138,12 @@ def npread(file_like, dtype, n):
     """
     dtype = np.dtype(dtype)
     
-    # np.readfile is not able to read from gzopene-d file
-    alldata = file_like.read(dtype.itemsize * n)
+    alldata = fh_in.read(dtype.itemsize * n_elements)
     
-    if len(alldata) != dtype.itemsize * n:
+    if len(alldata) != dtype.itemsize * n_elements:
         raise EOFError('End of file reached before number of results parsed')
-    readdata=np.frombuffer(alldata, dtype, n)
-    if readdata.size != n:
+    readdata=np.frombuffer(alldata, dtype, n_elements)
+    if readdata.size != n_elements:
         raise EOFError('End of file reached before number of results parsed')
     
     """
@@ -144,5 +157,30 @@ def npread(file_like, dtype, n):
     """
     
     return readdata
+
+
+
+@beartype
+def write_char(fh_out: BufferedWriter, output: str):
+    return fh_out.write(str.encode(output))
+
+
+@beartype
+def write_string(fh_out: BufferedWriter, output: str):
+    
+    """
+    num_bytes = read_byte(fh_in: BufferedReader)
+    num_chars = num_bytes % 128
+    shift = 0
+
+    while num_bytes // 128 == 1:
+        num_bytes = read_byte(fh_in: BufferedReader)
+        shift += 7
+        offset = (num_bytes % 128) * (2 ** shift)
+        num_chars += offset
+
+    return read_char(fh_in, num_chars)
+    """
+
 
 
