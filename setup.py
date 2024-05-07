339318868217c61e296a5f6a00c1580c76eb6acf
yhoogstrate <y.hoogstrate@gmail.com>
2024-05-07 09:06:36 +0200

    update

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-
-
-with open("README.md", 'r') as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r") as fh:
-    requirements = [_.strip() for _ in fh.readlines() if _[0] != "#"]
+exec(open('idattools/__init__.py').read())
 
 
 setup(
     name='idat-tools',
     scripts=['bin/idat-tools'],
     packages=["idattools"],
-    version='0.0',
+    version=__version__,
+    author=__author__,
+    url=__homepage__,
     description='Toolkit to read, modify and export idat files',
-    long_description=long_description,
-    author='Youri Hoogstrate',
-    url="https://github.com/yhoogstrate/idat-tools",
-    install_requires=['wheel']
+    long_description=open("README.md", 'r').read().strip(),
+    setup_requires=['setuptools'],# bit odd, this can only be loaded if it is there
+    install_requires=[_.strip() for _ in open("requirements.txt", "r").readlines() if _[0] != "#"],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ]
 )
+
